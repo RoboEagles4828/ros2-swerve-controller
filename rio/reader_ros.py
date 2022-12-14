569c6b04ce6af607c96398925b46f3ebafbfa0f6
SarangaR <sarnga.raj@gmail.com>
2022-12-13 20:04:31 -0500

    ros2 pkg

--- rio/reader_ros.py
+++ rio/reader_ros.py
@@ -8,9 +8,10 @@ class OrinSubscriber(Node):
     def __init__(self):
         super().__init__('orin_subscriber')
 
-        qos_profile = qos.QoSProfile(depth=1,reliability=qos.ReliabilityPolicy.BEST_EFFORT, durability=qos.DurabilityPolicy.VOLATILE)
+        qos_profile = qos.QoSProfile(depth=1, reliability=qos.ReliabilityPolicy.BEST_EFFORT, durability=qos.DurabilityPolicy.VOLATILE)
+        qos_profile_2 = qos.QoSProfile(depth=1, reliability=qos.ReliabilityPolicy.SYSTEM_DEFAULT, durability=qos.DurabilityPolicy.SYSTEM_DEFAULT)
 
-        self.subscription = self.create_subscription(Joy, 'joystick_data', self.listener_callback, qos_profile)
+        self.subscription = self.create_subscription(Joy, r'joystick_data', self.listener_callback, qos_profile_2)
 
     def listener_callback(self, msg):
         self.get_logger().info('Recieved: "%s"' % msg.data)
--- rio/ros2_connext_dds/src/writer.py
+++ rio/ros2_connext_dds/src/writer.py
@@ -1,5 +1,6 @@
 import rticonnextdds_connector as rti
 from time import sleep
+import cv2
 
 with rti.open_connector(config_name="ROS2_PARTICIPANT_LIB::publisher",
         url="./ROS_RTI.xml") as connector:
@@ -7,11 +8,10 @@ with rti.open_connector(config_name="ROS2_PARTICIPANT_LIB::publisher",
     output = connector.get_output("joystick_data_publisher::joystick_data_writer")
 
     print("Waiting for subscriptions...")
-    output.wait_for_subscriptions()
+    output.wait_for_subscriptions() 
 
     axes = [1, 2, 3, 4]
     buttons = [4, 3, 2, 1]
-
     while(True):
         output.instance.set_dictionary({"axes": axes, "buttons": buttons})
         output.write()
@@ -19,6 +19,3 @@ with rti.open_connector(config_name="ROS2_PARTICIPANT_LIB::publisher",
         print(f'sending\naxes: {axes}\nbuttons:{buttons}')
         axes = [n+1 for n in axes]
         buttons = [n+1 for n in buttons]
-
-    print('exiting')
-    connector.close()
