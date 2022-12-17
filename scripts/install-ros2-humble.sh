10a72740518f12a790aecb69e064bfc0091b9f55
gage miller <gagemiller155@gmail.com>
2022-12-17 15:19:14 -0500

    Add updated scripts for humble and new isaac sim

--- scripts/install-ros2-humble.sh
+++ scripts/install-ros2-humble.sh
@@ -9,36 +9,36 @@ set -eu
 # by Open Robotics, licensed under CC-BY-4.0
 # source: https://github.com/ros2/ros2_documentation
 
-CHOOSE_ROS_DISTRO=galactic
+CHOOSE_ROS_DISTRO=humble
 INSTALL_PACKAGE=desktop
-TARGET_OS=focal
+TARGET_OS=jammy
 
 # Check OS version
 if ! which lsb_release > /dev/null ; then
-	sudo apt-get update
-	sudo apt-get install -y curl lsb-release
+        sudo apt-get update
+        sudo apt-get install -y curl lsb-release
 fi
 
 if [[ "$(lsb_release -sc)" == "$TARGET_OS" ]]; then
-	echo "OS Check Passed"
+        echo "OS Check Passed"
 else
-	printf '\033[33m%s\033[m\n' "=================================================="
-	printf '\033[33m%s\033[m\n' "ERROR: This OS (version: $(lsb_release -sc)) is not supported"
-	printf '\033[33m%s\033[m\n' "=================================================="
-	exit 1
+        printf '\033[33m%s\033[m\n' "=================================================="
+        printf '\033[33m%s\033[m\n' "ERROR: This OS (version: $(lsb_release -sc)) is not supported"
+        printf '\033[33m%s\033[m\n' "=================================================="
+        exit 1
 fi
 
 if ! dpkg --print-architecture | grep -q 64; then
-	printf '\033[33m%s\033[m\n' "=================================================="
-	printf '\033[33m%s\033[m\n' "ERROR: This architecture ($(dpkg --print-architecture)) is not supported"
-	printf '\033[33m%s\033[m\n' "See https://www.ros.org/reps/rep-2000.html"
-	printf '\033[33m%s\033[m\n' "=================================================="
-	exit 1
+        printf '\033[33m%s\033[m\n' "=================================================="
+        printf '\033[33m%s\033[m\n' "ERROR: This architecture ($(dpkg --print-architecture)) is not supported"
+        printf '\033[33m%s\033[m\n' "See https://www.ros.org/reps/rep-2000.html"
+        printf '\033[33m%s\033[m\n' "=================================================="
+        exit 1
 fi
 
 # Install
 sudo apt-get update
-sudo apt-get install -y software-properties-common
+sudo apt-get install software-properties-common
 sudo add-apt-repository universe
 sudo apt-get install -y curl gnupg2 lsb-release build-essential
 
@@ -54,6 +54,8 @@ sudo rosdep init
 rosdep update
 grep -F "source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash" ~/.bashrc ||
 echo "source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash" >> ~/.bashrc
+grep -F "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" ~/.bashrc ||
+echo "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" >> ~/.bashrc
 
 set +u
 
--- scripts/use-isaac-ros2-bridge.sh
+++ scripts/use-isaac-ros2-bridge.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-ISAAC_VERSION="2022.1.1"
+ISAAC_VERSION="2022.2.0"
 basekit_file="/home/${USER}/.local/share/ov/pkg/isaac_sim-${ISAAC_VERSION}/apps/omni.isaac.sim.base.kit"
 
-has_ros2=$(grep ros2 $basekit_file)
+has_ros2_humble=$(grep ros2_bridge-humble $basekit_file)
 
-if [ -z "$has_ros2" ];
+if [ -z "$has_ros2_humble" ];
 then
-    echo "Setting ROS2 as default bridge"
-    sed -i 's/ros/ros2/' $basekit_file
+    echo "Setting ROS2 Humble as default bridge"
+    sed -i 's/ros_bridge/ros2_bridge-humble/' $basekit_file
 else
-    echo "ROS2 Already Default"
+    echo "ROS2 Humble Already Default"
 fi
