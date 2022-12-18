d748129c8d3b885817568252b8efe49a1107511a
gage miller <gagemiller155@gmail.com>
2022-12-18 06:39:32 +0000

    Add updates for humble + isaac

--- src/swerve_hardware/include/swerve_hardware/test_drive.hpp
+++ src/swerve_hardware/include/swerve_hardware/test_drive.hpp
@@ -24,13 +24,14 @@
 #include "hardware_interface/hardware_info.hpp"
 #include "hardware_interface/system_interface.hpp"
 #include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "rclcpp/clock.hpp"
+#include "rclcpp/duration.hpp"
 #include "rclcpp/macros.hpp"
+#include "rclcpp/time.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 #include "rclcpp_lifecycle/state.hpp"
-#include "swerve_hardware/visibility_control.h"
-#include "rclcpp/rclcpp.hpp"
 
-using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+#include "swerve_hardware/visibility_control.h"
 
 namespace swerve_hardware
 {
@@ -40,7 +41,7 @@ public:
   RCLCPP_SHARED_PTR_DEFINITIONS(TestDriveHardware)
 
   SWERVE_HARDWARE_PUBLIC
-  CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
+  hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
 
   SWERVE_HARDWARE_PUBLIC
   std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
@@ -49,16 +50,16 @@ public:
   std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
 
   SWERVE_HARDWARE_PUBLIC
-  CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
+  hardware_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
 
   SWERVE_HARDWARE_PUBLIC
-  CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
+  hardware_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
 
   SWERVE_HARDWARE_PUBLIC
-  hardware_interface::return_type read() override;
+  hardware_interface::return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
   SWERVE_HARDWARE_PUBLIC
-  hardware_interface::return_type write() override;
+  hardware_interface::return_type write(const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
 private:
   // Store the command for the simulated robot
