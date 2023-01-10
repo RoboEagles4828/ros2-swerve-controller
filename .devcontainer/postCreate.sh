7b0404c248537b7f87be86a592ad6193caf47356
gage miller <gagemiller155@gmail.com>
2023-01-10 15:19:16 +0000

    Configure local cache using toml file

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -11,9 +11,12 @@ sudo chown -R $USER:$USER /isaac-sim/kit/logs
 sudo chown $USER:$USER /isaac-sim/exts
 sudo chown -R $USER:$USER /isaac-sim/exts/omni.isaac.synthetic_recorder
 
+# Configure omniverse cache 
+cp ./.devcontainer/omniverse.toml /home/vscode/.nvidia-omniverse/config/omniverse.toml
+
 # Run the postinstall script for isaac sim
 # Check to see if post install has already been run
-if [[ ! -d ~/.cache/ov/Kit ]]; then
+if [ ! -d ~/.cache/ov/Kit ]; then
     echo "ISAAC SIM POST INSTALL RUNNING... THIS WILL TAKE ABOUT 10min"
     bash /isaac-sim/omni.isaac.sim.post.install.sh
 fi                                                                                                                                                                                                                                                                                                      
