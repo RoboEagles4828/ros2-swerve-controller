768a25ce8165ce281e7c473da6d244aa7f5e8b47
Gage Miller <gagemiller155@gmail.com>
2023-01-14 00:24:10 +0000

    Fix install script

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
+USERNAME=vscode
+
 # Take ownership of persistent data folders
-sudo chown -R $USER:$USER ~/.cache
-sudo chown -R $USER:$USER ~/.local
-sudo chown -R $USER:$USER ~/.nv
-sudo chown -R $USER:$USER ~/.nvidia-omniverse
+sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.cache
+sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.local
+sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.nv
+sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.nvidia-omniverse
 
 # Take ownership of isaac-sim folders
-sudo chown $USER:$USER /isaac-sim/kit
-sudo chown -R $USER:$USER /isaac-sim/kit/logs
-sudo chown $USER:$USER /isaac-sim/exts
-sudo chown -R $USER:$USER /isaac-sim/exts/omni.isaac.synthetic_recorder
+sudo chown $USERNAME:$USERNAME /isaac-sim/kit
+sudo chown -R $USERNAME:$USERNAME /isaac-sim/kit/logs
+sudo chown $USERNAME:$USERNAME /isaac-sim/exts
+sudo chown -R $USERNAME:$USERNAME /isaac-sim/exts/omni.isaac.synthetic_recorder
 
 # Configure omniverse cache 
-cp ./.devcontainer/omniverse.toml /home/vscode/.nvidia-omniverse/config/omniverse.toml
+cp ./.devcontainer/omniverse.toml /home/$USERNAME/.nvidia-omniverse/config/omniverse.toml
 
 # Run the postinstall script for isaac sim
 # Check to see if post install has already been run
